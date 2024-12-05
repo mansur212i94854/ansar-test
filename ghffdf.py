3b5c25e50922ebb193e2c7b555fd26eb38453bad
mansur212i94854 <mtutuev7@gmail.com>
2024-12-05 15:52:37 +0300

    первая иницилизация

---
+++ ghffdf.py
@@ -0,0 +1,93 @@
+# from datetime import datetime
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S")
+# print("Текущее время =", current_time)
+
+import time
+#
+# print("Ждем 5 секунд")
+# time.sleep(5)
+# print("Надоело ждать 5 секунд")
+
+
+# import datetime as d, sys, os, platform
+#
+# print(platform.system())
+#
+#
+# import math
+# print(math.pi * int(input('Введите радиус: ')) ** 3)
+
+
+#
+# import math
+#
+# chislo = float(input("Введите число: "))
+# chislo_sqrt = math.sqrt(chislo)
+# print(chislo_sqrt)
+
+# import random
+#
+# print(random.randint(1, 100))
+
+# import random
+# x = random.randint(1, 100)
+# zapros = int(input('Введите предполагаемое число: '))
+#
+# if zapros > x:
+#     print('Ваша число баольше икса')
+# elif zapros < x:
+#     print('Ваша число меньше икса')
+# else:
+#     print('Угадал')
+
+
+# import math
+# import time
+#
+# start = time.time()
+#
+# a = int(input('Введите число: '))
+# print(math.factorial(a))
+#
+# konec = time.time()
+#
+# rezult = konec - start
+#
+# print("Код выполнен за: ", rezult)
+
+
+#
+# import random
+#
+# password = ''
+# for _ in range(8):
+#     a = random.randint(1, 100)
+#     password += str(a)
+# print(password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
